9a46fca418378a7978f763fcb2d047aeb24f8c08
Jussi Hurtta <H9111@Labranet.jamk.fi>
2017-09-18 10:21:52 +0300

    Initial commit

---
+++ app/src/main/java/fi/jamk/imageview/MainActivity.java
@@ -0,0 +1,45 @@
+package fi.jamk.imageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // get views
+        imageView = (ImageView)findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.textView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        // start showing images
+        imageIndex = 0;
+        showImage();
+
+    }
+
+    // image view object
+    private ImageView imageView;
+    // text view object
+    private TextView textView;
+    // progress bar object
+    private ProgressBar progressBar;
+    // example image's path (change to your own if needed...)
+    private final String PATH = "http://domain.fi/android/";
+    // example image names (change to your own if needed...)
+    private String[] images = {"image1.jpg","image2.jpg","image3.jpg"};
+    // which image is now visible
+    private int imageIndex;
+    // async task to load a new image
+    private DownloadImageTask task;
+    // swipe down and up values
+    private float x1,x2;
+
+    public void showImage() {
+        // create a new AsyncTask object
+        task = new DownloadImageTask();
+        // start asynctask
+        task.execute(PATH + images[imageIndex]);
+    }
+}
